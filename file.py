daa5881b64ae0efc0e2a52ed06b87a2f9430ff52
X E R O T I C <muhammadparvez762015@gmail.com>
2026-06-15 18:23:03 +0600

    Update file.py

--- file.py
+++ file.py
@@ -1,7 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/python3.13
 import tom
+
 if __name__ == "__main__":
-    if callable(getattr(tom, 'main', None)):
-        tom.main()
-    else:
-        print("tom :", [x for x in dir(tom) if not x.startswith('_')])
+    tom.__ApV__()
